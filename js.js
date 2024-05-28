bd4d2d13586ccac4b17bec52ba16fe9dd15c03a7
jal-morales <jal.morales89@gmail.com>
2024-05-27 23:21:40 -0400

    Se deja todo Resposivo y se agrega la descripcion del index con el DOM

---
+++ js.js
@@ -0,0 +1,3 @@
+let CabioDato =document.getElementById("tituloIndex");
+
+CabioDato.before("En JuegosJP encontrarás una amplia variedad de juegos de mesa para todas las edades y gustos. Explora nuestras categorías y descubre juegos emocionantes y divertidos que serán el centro de atención en tus reuniones y momentos de ocio.");
